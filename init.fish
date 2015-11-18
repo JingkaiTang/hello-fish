b482b5f3c90f224846035e00e5a385cd1967bc94
Jingkai Tang <jingkaitang@gmail.com>
2015-11-18 16:49:31 +0800

    fixed: inits script need to be loaded after all other scripts

--- init.fish
+++ init.fish
@@ -9,21 +9,45 @@
 #
 # Add directories here for fish scripts of $HELLO_FISH_HOME
 #
-# inits: initial work for some function, e.g. rvm
 # env: environment variables, e.g. alias, PATH
 # functions: fish functions
-set fish_home_scripts_dirs inits env functions
+set home_scripts_dirs env functions
 
 #
 # Add other path fish scripts here
 #
 set other_scripts_dirs
 
-# Join two scripts dirs in one variable
-set scripts_dirs $HELLO_FISH_HOME/$fish_home_scripts_dirs $other_scripts_dirs
+# Join two scripts dirs into one variable
+set scripts_dirs $HELLO_FISH_HOME/$home_scripts_dirs $other_scripts_dirs
 
 # Load Scripts
 for dir in $scripts_dirs
+    for script in $dir/*.fish
+        source $script
+    end
+end
+
+#
+# Init Scripts for after all settings
+#
+
+#
+# Add directories here for fish initial scripts of $HELLO_FISH_HOME
+#
+# inits: initial work for some function, e.g. rvm
+set home_inits_dirs inits
+
+#
+# Add directories here for other path fish initial scripts
+#
+set other_inits_dirs
+
+# Join two inits dirs into one variable
+set inits_dirs $HELLO_FISH_HOME/$home_inits_dirs $other_inits_dirs
+
+# Load Scripts
+for dir in $inits_dirs
     for script in $dir/*.fish
         source $script
     end
