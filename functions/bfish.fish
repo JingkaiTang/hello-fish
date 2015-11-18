41d77e926a6426dee53b7aaf8921211b54b05ace
Jingkai Tang <jingkaitang@gmail.com>
2015-11-18 19:22:02 +0800

    added: helpful function and init script for oh-my-fish theme bobthefish

---
+++ functions/bfish.fish
@@ -0,0 +1,9 @@
+function bfish -d "oh-my-fish theme bobthefish helpful function"
+    if test $argv[1] = "ruby"
+        if test $theme_display_ruby = "yes"
+            set -g theme_display_ruby no
+        else
+            set -g theme_display_ruby yes
+        end
+    end
+end
---
+++ inits/bobthefish_init.fish
@@ -0,0 +1,3 @@
+# oh-my-fish theme bobthefish
+set -g theme_display_hg yes
+set -g theme_display_ruby no
