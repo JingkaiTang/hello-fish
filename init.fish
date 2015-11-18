26dafbbd2c1aaa9c6c2f0c346e7b34c446873a5b
Jingkai Tang <jingkaitang@gmail.com>
2015-11-18 16:33:23 +0800

    added: functions rvm, rvm_init

--- init.fish
+++ init.fish
@@ -1,7 +1,31 @@
-# Hello Fish
+# 
+# Hello Fish Init File
+#
 
-# Env Variables
-for fish_script in $HELLO_FISH/env/*.fish
-    source $fish_script
+#
+# Scripts Dirs
+#
+ 
+#
+# Add directories here for fish scripts of $HELLO_FISH_HOME
+#
+# inits: initial work for some function, e.g. rvm
+# env: environment variables, e.g. alias, PATH
+# functions: fish functions
+set fish_home_scripts_dirs inits env functions
+
+#
+# Add other path fish scripts here
+#
+set other_scripts_dirs
+
+# Join two scripts dirs in one variable
+set scripts_dirs $HELLO_FISH_HOME/$fish_home_scripts_dirs $other_scripts_dirs
+
+# Load Scripts
+for dir in $scripts_dirs
+    for script in $dir/*.fish
+        source $script
+    end
 end
 
