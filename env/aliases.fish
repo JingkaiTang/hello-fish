3eb4676bc03858ad2e85a342730aef1fa6f98512
Jingkai Tang <jingkaitang@gmail.com>
2015-11-29 13:18:59 +0800

    updated: alias with test exits

--- env/aliases.fish
+++ env/aliases.fish
@@ -1,2 +1,14 @@
-alias sudo "sudo -E"
-alias vi "vim"
+# sudo
+set tmp (type sudo)
+if test -n tmp 
+    alias sudo "sudo -E"
+end
+set -e tmp
+
+# vi
+set tmp (type vi ^ /dev/null)
+if test -z tmp
+    alias vi "vim"
+end
+set -e tmp
+
