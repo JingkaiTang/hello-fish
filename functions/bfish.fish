621349ba04a192aa1bd455761701d8fe79a86d8e
Jingkai Tang <jingkaitang@gmail.com>
2015-12-04 14:23:22 +0800

    updated: bfish with user info switch

--- functions/bfish.fish
+++ functions/bfish.fish
@@ -1,9 +1,22 @@
+# bfish function
 function bfish -d "oh-my-fish theme bobthefish helpful function"
-    if test $argv[1] = "ruby"
-        if test $theme_display_ruby = "yes"
-            set -g theme_display_ruby no
-        else
-            set -g theme_display_ruby yes
+    for arg in $argv
+        if test $arg = "ruby"
+            if test -z "$theme_display_ruby"
+                set -g theme_display_ruby yes
+            else if test $theme_display_ruby = "yes"
+                set -g theme_display_ruby no
+            else
+                set -g theme_display_ruby yes
+            end
+        else if test $arg = "user"
+            if test -z "$theme_display_user"
+                set -g theme_display_user yes
+            else if test $theme_display_user = "yes"
+                set -g theme_display_user no
+            else
+                set -g theme_display_user yes
+            end
         end
     end
     return 0
