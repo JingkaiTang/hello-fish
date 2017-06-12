c31334fc5ae40d0f1516de29ce51f468d35874dc
Jingkai Tang <jingkaitang@qq.com>
2017-06-12 15:21:51 +0800

    mix: rm cnpm, fix npm path

--- platforms/acer-libido-archlinux/npm.fish
+++ platforms/acer-libido-archlinux/npm.fish
@@ -2,7 +2,7 @@
 set -xg NPM_PACKAGES "$HOME/.npm-packages"
 
 # Tell our environment about user-installed node tools
-set -xg PATH $PATH "$NPM_PACKAGES/bin"
+set -xg PATH "$NPM_PACKAGES/bin" $PATH 
 
 # Tell Node about these packages
 set -xg NODE_PATH "$NPM_PACKAGES/lib/node_modules"
